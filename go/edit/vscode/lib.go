656ea4f7e0208c24b224b48f677997178c54ed8e
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-04-12 23:39:50 +0900

    test ok

--- go/edit/vscode/lib.go
+++ go/edit/vscode/lib.go
@@ -1,9 +1,11 @@
 package vscode
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"syscall"
+	"unicode/utf8"
 )
 
 // Same as VS Code extention API's Position
@@ -18,7 +20,8 @@ type Range struct {
 }
 
 type FileHandler struct {
-	file *os.File
+	file       *os.File
+	readWriter *bufio.ReadWriter
 }
 
 func NewFileHandler(filename string) (*FileHandler, error) {
@@ -27,9 +30,53 @@ func NewFileHandler(filename string) (*FileHandler, error) {
 		return nil, fmt.Errorf("NewFileHandler failed, %s", err)
 	}
 
+	rw := bufio.NewReadWriter(bufio.NewReader(f), bufio.NewWriter(f))
+
 	return &FileHandler{
-		file: f,
+		file:       f,
+		readWriter: rw,
 	}, nil
+}
+
+func (f *FileHandler) byteIndexAt(position Position) (int, error) {
+	if position.Line < 0 {
+		return 0, fmt.Errorf("argument position.Line = %d, but it cannnot be a negative number", position.Line)
+	}
+
+	if position.Character < 0 {
+		return 0, fmt.Errorf("argument position.Character = %d, but it cannnot be a negative number", position.Character)
+	}
+
+	var totalBytes int
+	var line []byte
+
+	// read up to the prev line of position.Line
+	for i := 0; i < position.Line; i++ {
+		line, _, err := f.readWriter.ReadLine()
+		if err != nil {
+			return 0, err
+		}
+		totalBytes += len(line)
+		fmt.Println("aaa")
+	}
+
+	// read the position.Line
+	line, _, err := f.readWriter.ReadLine()
+	if err != nil {
+		return 0, err
+	}
+
+	// read the position.Line up to the position.Character
+	for i := 0; i <= position.Character; {
+		_, size := utf8.DecodeRune(line[i:])
+		if size == 0 {
+			return 0, fmt.Errorf("argument position.Character = %d, but it cannnot be greater than %d, the number of characters at line = %d", position.Character, i, position.Line)
+		}
+		i += size
+		totalBytes += size
+	}
+
+	return totalBytes, nil
 }
 
 func (f *FileHandler) Insert(position Position, newText string) error {
