d895275006c510406db695724e098364a2892b48
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-05-13 22:59:04 +0900

    remove unused code

--- go/edit/vscode/lib.go
+++ go/edit/vscode/lib.go
@@ -80,60 +80,6 @@ func (r Range) Validate() error {
 	return nil
 }
 
-func NewFileHandler(filename string) (*FileHandler, error) {
-	f, err := os.OpenFile(filename, syscall.O_RDWR, 0666)
-	if err != nil {
-		return nil, fmt.Errorf("NewFileHandler failed, %s", err)
-	}
-
-	return &FileHandler{
-		file: f,
-	}, nil
-}
-
-func (f *FileHandler) offset(position Position) (int, error) {
-	if err := position.Validate(); err != nil {
-		return 0, fmt.Errorf("offset() error, %s", err)
-	}
-
-	r := bufio.NewReader(f.file)
-
-	var totalBytes int
-	var line []byte
-
-	// read up to the prev line of position.Line
-	for i := 0; i < position.Line; i++ {
-		line, _, err := r.ReadLine()
-		if err != nil {
-			return 0, err
-		}
-		totalBytes += len(line)
-	}
-
-	// read the position.Line
-	line, _, err := r.ReadLine()
-	if err != nil {
-		return 0, err
-	}
-
-	// read the position.Line up to the position.Character
-	for i := 0; i <= position.Character; {
-		_, size := utf8.DecodeRune(line[i:])
-		if size == 0 {
-			return 0, fmt.Errorf("argument position.Character = %d, but it cannnot be greater than %d, the number of characters at line = %d", position.Character, i, position.Line)
-		}
-		i += size
-		totalBytes += size
-	}
-
-	_, err = f.file.Seek(0, 0)
-	if err != nil {
-		return 0, errors.New("failed to reset offset at the end")
-	}
-
-	return totalBytes, nil
-}
-
 // Copy lines from fromReader up to uptoLine (zero-based), and copy the lines to toBuilder.
 // from must be set to the initial position of the input, otherwise the behavior is not guaranteed.
 // uptoLine must be zero or a positive number.
@@ -393,37 +339,4 @@ func Delete(filename string, delRange Range) error {
 	// f.WriteAt([]byte(builder.String()), 0)
 
 	return nil
-}
-
-func (f *FileHandler) Insert2(position Position, newText string) error {
-	offset, err := f.offset(position)
-	if err != nil {
-		return fmt.Errorf("Insert() failed, %s", err)
-	}
-
-	oldText := make([]byte, len(newText))
-	n, err := f.file.ReadAt(oldText, int64(offset))
-	if err != nil {
-		return fmt.Errorf("Insert failed, %s", err)
-	}
-	if n != len(newText) {
-		return fmt.Errorf("Insert failed, couldn't read %d bytes upon preparation", len(newText))
-	}
-
-	updatedText := string(oldText) + newText
-	_, err = f.file.WriteAt([]byte(updatedText), int64(offset))
-	if err != nil {
-		return fmt.Errorf("Insert() failed, %s", err)
-	}
-
-	// f. filepointer
-	return nil
-}
-
-func (f *FileHandler) Delete(position Position, newText string) error {
-	return nil
-}
-
-func (f *FileHandler) Close() error {
-	return f.file.Close()
 }
--- go/edit/vscode/lib_test.go
+++ go/edit/vscode/lib_test.go
@@ -379,26 +379,6 @@ Good morning` + "\n",
 	}
 }
 
-func TestSeek(t *testing.T) {
-	h, err := NewFileHandler("testdata/test.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = h.offset(Position{Line: 2, Character: 3})
-	if err != nil {
-		t.Fatal(err)
-	}
-	// do it again and same result
-	i, err := h.offset(Position{Line: 2, Character: 3})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if i != 8 {
-		t.Fatalf("expected %d but got %d", 5, i)
-	}
-}
-
 // func TestInsert(t *testing.T) {
 // 	err := Insert("testdata/test.txt", Position{Line: 2, Character: 3}, "abc")
 // 	if err != nil {
