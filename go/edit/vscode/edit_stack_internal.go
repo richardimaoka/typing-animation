079992191dd6cfc51ade0946d3ebf3548a09b0fd
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-06-02 13:57:22 +0900

    word by wrod

--- go/edit/vscode/edit_stack_internal.go
+++ go/edit/vscode/edit_stack_internal.go
@@ -12,15 +12,17 @@ import (
 //
 // If line has '\n', this returns an error
 // If line is empty, this should return the count of zero
-func countRunesInLine(line []byte) (int, error) {
-	if len(line) == 0 {
+func countRunesInLine(lineString string) (int, error) {
+	if len(lineString) == 0 {
 		return 0, nil
 	}
+
+	lineBytes := []byte(lineString)
 
 	byteOffset := 0
 	runeCount := 0
 	for ; ; runeCount++ {
-		r, size := utf8.DecodeRune(line[byteOffset:])
+		r, size := utf8.DecodeRune(lineBytes[byteOffset:])
 		if r == '\n' {
 			return 0, errors.New("encountered new-line")
 		}
@@ -39,16 +41,13 @@ func countRunesInLine(line []byte) (int, error) {
 	return runeCount, nil
 }
 
-func countRunesInLineS(line string) (int, error) {
-	return countRunesInLine([]byte(line))
-}
-
+// Calculate the positoin after adding newText
 func positionAfterAdd(currentPos Position, newText string) (Position, error) {
 	linesToAdd := strings.Split(newText, "\n")
 
 	if len(linesToAdd) == 1 {
 		line := linesToAdd[0]
-		runeCount, err := countRunesInLineS(line)
+		runeCount, err := countRunesInLine(line)
 		if err != nil {
 			return Position{}, fmt.Errorf("failed to calculate new position after add %s", err)
 		}
@@ -61,7 +60,7 @@ func positionAfterAdd(currentPos Position, newText string) (Position, error) {
 	} else {
 		lastLine := linesToAdd[len(linesToAdd)-1]
 
-		runeCount, err := countRunesInLineS(lastLine)
+		runeCount, err := countRunesInLine(lastLine)
 		if err != nil {
 			return Position{}, fmt.Errorf("failed to calculate new position after add %s", err)
 		}
@@ -77,17 +76,22 @@ func positionAfterAdd(currentPos Position, newText string) (Position, error) {
 // 	return nil
 // }
 
-func addCharByChar(currentPos Position, lineString string) ([]EditInsert, error) {
-	line := []byte(lineString)
-
+// Return []EditInsert slice, split by char, to add line from currentPos
+// line should not contain \'\n'
+//
+// If line contains '\n', this returns an error
+// If line is empty, this should return the count of zero
+func addCharByChar(currentPos Position, line string) ([]EditInsert, error) {
 	if len(line) == 0 {
 		return []EditInsert{}, nil
 	}
 
+	lineBytes := []byte(line)
+
 	edits := []EditInsert{}
 	byteOffset := 0
 	for c := 0; ; c++ {
-		r, size := utf8.DecodeRune(line[byteOffset:])
+		r, size := utf8.DecodeRune(lineBytes[byteOffset:])
 		if r == '\n' {
 			return nil, errors.New("encountered new-line")
 		}
@@ -114,6 +118,35 @@ func addCharByChar(currentPos Position, lineString string) ([]EditInsert, error)
 	return edits, nil
 }
 
-// func addLineByLine(newText string) []string {
-// 	return nil
-// }
+// Return []EditInsert slice, split by word, to add line from currentPos
+// line should not contain \'\n'
+//
+// If line contains '\n', this returns an error
+// If line is empty, this should return the count of zero
+func addWordByWord(currentPos Position, lineString string) ([]EditInsert, error) {
+	pos := currentPos
+
+	if len(lineString) == 0 {
+		return []EditInsert{}, nil
+	}
+
+	lineWords := strings.SplitAfter(lineString, " ")
+
+	edits := []EditInsert{}
+	for _, word := range lineWords {
+		edits = append(edits,
+			EditInsert{
+				NewText:  word,
+				Position: pos,
+			},
+		)
+
+		c, err := countRunesInLine(word)
+		if err != nil {
+			return nil, err
+		}
+		pos.Character = pos.Character + c // pos.Line remain same
+	}
+
+	return edits, nil
+}
--- go/edit/vscode/edit_stack_internal_test.go
+++ go/edit/vscode/edit_stack_internal_test.go
@@ -21,8 +21,7 @@ func TestCountRunesInLine(t *testing.T) {
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			line := []byte(c.line)
-			result, err := countRunesInLine(line)
+			result, err := countRunesInLine(c.line)
 			if err != nil {
 				if c.err {
 					return // expected error
@@ -109,6 +108,58 @@ func TestAddCharByChar(t *testing.T) {
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
 			result, err := addCharByChar(c.currentPos, c.newText)
+			if err != nil {
+				if c.err {
+					return // expected error
+				}
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if c.err {
+				t.Fatalf("Expected error: but succeeded with result = %+v", result)
+			}
+
+			diff := cmp.Diff(c.expected, result)
+			if len(diff) > 0 {
+				t.Errorf("%s", diff)
+			}
+		})
+	}
+}
+
+func TestAddWordByWord(t *testing.T) {
+	cases := map[string]struct {
+		newText    string
+		currentPos Position
+		expected   []EditInsert
+		err        bool
+	}{
+		"this is a sentence.": {
+			/**/ "this is a text.",
+			Position{Line: 3, Character: 10},
+			[]EditInsert{
+				{NewText: "this ", Position: Position{Line: 3, Character: 10}},
+				{NewText: "is ", Position: Position{Line: 3, Character: 15}},
+				{NewText: "a ", Position: Position{Line: 3, Character: 18}},
+				{NewText: "text.", Position: Position{Line: 3, Character: 20}},
+			},
+			false},
+		"sentence with a wrong   spacing": {
+			"a b c",
+			Position{Line: 3, Character: 10},
+			[]EditInsert{
+				{NewText: "a ", Position: Position{Line: 3, Character: 10}},
+				{NewText: "b ", Position: Position{Line: 3, Character: 12}},
+				{NewText: "c", Position: Position{Line: 3, Character: 14}},
+			},
+			false},
+		"ERROR: new line at the end":    {"0123456789\n", Position{Line: 3, Character: 10}, nil, true},
+		"ERROR: new line in the middle": {"0123456789\n012三四", Position{Line: 3, Character: 10}, nil, true},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			result, err := addWordByWord(c.currentPos, c.newText)
 			if err != nil {
 				if c.err {
 					return // expected error
