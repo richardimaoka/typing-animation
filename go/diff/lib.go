8c1e2ff7b813d7b2adc12784958ddd12cb18c7e9
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-14 00:38:03 +0900

    tests

--- go/diff/lib.go
+++ go/diff/lib.go
@@ -1,11 +1,41 @@
 package diff
 
-import "github.com/richardimaoka/typing-animation/go/edit/vscode"
+import (
+	"fmt"
 
-func CreateStack(before, after string) *vscode.EditStack {
-	return nil
+	"github.com/richardimaoka/typing-animation/go/edit/vscode"
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+func createStack(before, after string) *vscode.EditStack {
+	stack := vscode.NewEditStack()
+	dmp := diffmatchpatch.New()
+
+	diffs := dmp.DiffMain(before, after, true)
+
+	for _, d := range diffs {
+		switch d.Type {
+		case diffmatchpatch.DiffDelete:
+			stack.AppendDelete(d.Text)
+		case diffmatchpatch.DiffEqual:
+			stack.AppendEqual(d.Text)
+		case diffmatchpatch.DiffInsert:
+			stack.AppendInsert(d.Text)
+		default:
+			panic(fmt.Sprintf("unexpected diff match patch type = %d (%s)", d.Type, d.Type.String()))
+		}
+	}
+
+	return stack
 }
 
 func CalcEdits(before, after string) ([]vscode.Edit, error) {
-	return nil, nil
+	stack := createStack(before, after)
+
+	edits, err := stack.CalcEdits()
+	if err != nil {
+		return nil, fmt.Errorf("diff.CalcEdits failed, %s", err)
+	}
+
+	return edits, nil
 }
