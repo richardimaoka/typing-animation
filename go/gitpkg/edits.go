403c91436aad37a75cf2607a1cb2336663445ead
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-18 20:53:22 +0900

    edits

---
+++ go/gitpkg/edits.go
@@ -0,0 +1,7 @@
+package gitpkg
+
+import "github.com/richardimaoka/typing-animation/go/edit/vscode"
+
+func EditsBetweenCommits(orgname, reponame, filePath, beforeCommit, afterCommit string) ([]vscode.Edit, error) {
+	return nil, nil
+}
