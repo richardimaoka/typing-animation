466baa0a25312de27c479deb381e0637bf8220f7
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-23 04:49:02 +0900

    Right

--- next/app/components/Right.tsx
+++ next/app/components/Right.tsx
@@ -1,8 +1,30 @@
 import { SourceCodeEditor } from "./monaco/SourceCodeEditor";
 import styles from "./Right.module.css";
+import fs from "node:fs";
 
 interface Props {}
 
-export function Right(props: Props) {
-  return <SourceCodeEditor editorText="aa" language="go" />;
+export async function Right(props: Props) {
+  const editorText = fs.readFileSync(
+    process.cwd() + "/app/components/data/command.9334a46.go.txt",
+    "utf8"
+  );
+
+  const editsText = fs.readFileSync(
+    process.cwd() + "/app/components/data/51f06c7-9334a46.edits.json",
+    "utf8"
+  );
+
+  const edits = JSON.parse(editsText);
+
+  return (
+    <SourceCodeEditor
+      editorText={editorText}
+      language="go"
+      typingAnimation={{
+        editSequence: edits,
+        newEditorText: "",
+      }}
+    />
+  );
 }
